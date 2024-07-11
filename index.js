204473b5ef4e239915b14850615e1e5f13281ba4
Mohamed Ragab <rmidoragab222@gmail.com>
2024-07-11 23:32:06 +0300

    edit in url

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ dotenv.config({ path: path.join(__dirname, "./config/.env") });
 
 const app = express();
 const port = process.env.PORT;
-const baseUrl = process.env.BASE_URL;
 app.use(express.json());
 
-app.use(`${baseUrl}/auth`, authRouter);
-app.use(`${baseUrl}/category`, categoryRouter);
+app.use("/auth", authRouter);
+app.use("/category", categoryRouter);
 
 app.use("*", (req, res, next) => {
   res.status(404).json("404 Error Page Not Found");
