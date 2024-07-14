70bdbdc76be7592facb94783532427e8f492c339
Mohamed Ragab <rmidoragab222@gmail.com>
2024-07-14 06:37:37 +0300

    some fixes

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -31,7 +31,7 @@ router.delete("/delete/:taskId", auth(), deleteTask); // Delete tasks
 router.put("/update/:taskId", auth(), updateTask); // Update task
 router.patch("/toggleTaskSharing/:taskId", auth(), toggleTaskSharing); // Toggle task sharing
 router.get("/sharedtasks", getSharedTasks); // Show shared tasks
-router.get("/tasksbycategory", filterTasksByCategory); //filter tasks by category
-router.get("/tasksbyprivacy", filterTasksByPrivacy); //filter tasks by privacy
+router.get("/tasksbycategory", auth(), filterTasksByCategory); //filter tasks by category
+router.get("/tasksbyprivacy", auth(), filterTasksByPrivacy); //filter tasks by privacy
 
 export default router;
